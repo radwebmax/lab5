24ef77dc18aa80557c58240c10dbc80d3d54671a
Max <68424156+radwebmax@users.noreply.github.com>
2020-11-23 21:47:40 +0200

    Add files via upload

---
+++ lab5.2.py
@@ -0,0 +1,18 @@
+import unittest
+import app as tested_app
+import json
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = tested_app.app.test_client()
+
+    def test_get_hello_endpoint(self):
+        r = self.app.get('/')
+        self.assertEqual(r.status, '200 ok')
+        self.assertEqual(r.data, b'Hello world!')
+
+if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
